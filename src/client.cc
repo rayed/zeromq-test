dce9c069447d8226707bb2540bd17284ece2b1b5
Rayed Alrashed <rayed@rayed.com>
2016-06-29 05:02:18 +0300

    Use push/pull model

--- src/client.cc
+++ src/client.cc
@@ -1,10 +1,4 @@
-//
-//  Weather update client in C++
-//  Connects SUB socket to tcp://localhost:5556
-//  Collects weather updates and finds avg temp in zipcode
-//
-//  Olivier Chamoux <olivier.chamoux@fr.thalesgroup.com>
-//
+
 #include <zmq.hpp>
 #include <iostream>
 #include <sstream>
@@ -14,31 +8,14 @@ int main (int argc, char *argv[])
     zmq::context_t context (1);
 
     //  Socket to talk to server
-    std::cout << "Collecting updates from weather server…\n" << std::endl;
-    zmq::socket_t subscriber (context, ZMQ_SUB);
-    subscriber.connect("tcp://localhost:5556");
+    std::cout << "Started ... \n" << std::endl;
+    zmq::socket_t socket (context, ZMQ_PULL);
+    // socket.connect("tcp://localhost:5556");
+    socket.connect("ipc://weather.ipc");
 
-    //  Subscribe to zipcode, default is NYC, 10001
-    const char *filter = (argc > 1)? argv [1]: "10001 ";
-    subscriber.setsockopt(ZMQ_SUBSCRIBE, filter, strlen (filter));
-
-    //  Process 100 updates
-    int update_nbr;
-    long total_temp = 0;
-    for (update_nbr = 0; update_nbr < 100; update_nbr++) {
-
-        zmq::message_t update;
-        int zipcode, temperature, relhumidity;
-
-        subscriber.recv(&update);
-
-        std::istringstream iss(static_cast<char*>(update.data()));
-        iss >> zipcode >> temperature >> relhumidity ;
-
-        total_temp += temperature;
+    while(1) {
+        zmq::message_t message;
+        socket.recv(&message);
     }
-    std::cout     << "Average temperature for zipcode '"<< filter
-                <<"' was "<<(int) (total_temp / update_nbr) <<"F"
-                << std::endl;
     return 0;
 }
--- src/server.cc
+++ src/server.cc
@@ -1,46 +1,23 @@
-//
-//  Weather update server in C++
-//  Binds PUB socket to tcp://*:5556
-//  Publishes random weather updates
-//
-//  Olivier Chamoux <olivier.chamoux@fr.thalesgroup.com>
-//
+
 #include <zmq.hpp>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-#if (defined (WIN32))
-#include <zhelpers.hpp>
-#endif
-
-#define within(num) (int) ((float) num * random () / (RAND_MAX + 1.0))
+#include <iostream>
 
 int main () {
 
     //  Prepare our context and publisher
     zmq::context_t context (1);
-    zmq::socket_t publisher (context, ZMQ_PUB);
-    publisher.bind("tcp://*:5556");
-    publisher.bind("ipc://weather.ipc");                // Not usable on Windows.
+    zmq::socket_t socket (context, ZMQ_PUSH);
+    // socket.bind("tcp://*:5556");
+    socket.bind("ipc://weather.ipc");
 
-    //  Initialize random number generator
-    srandom ((unsigned) time (NULL));
-    for (int i=0 ; i < 1e5; i++) {
-
-        int zipcode, temperature, relhumidity;
-
-        //  Get values that will fool the boss
-        zipcode     = within (100000);
-        temperature = within (215) - 80;
-        relhumidity = within (50) + 10;
-
-        //  Send message to all subscribers
-        zmq::message_t message(20);
-        snprintf ((char *) message.data(), 20 ,
-            "%05d %d %d", zipcode, temperature, relhumidity);
-        publisher.send(message);
-
+    std::cout << "Sending: " << std::fixed << 1e6 << " messages\n";
+    for (int i=0 ; i < 1e6; i++) {
+        zmq::message_t message(5);
+        strcpy((char *) message.data(), "hello");
+        socket.send(message);
     }
     return 0;
 }
